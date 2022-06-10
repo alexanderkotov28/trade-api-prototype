3ed75541735f26799655a683b49d0d5a56348f4a
Alexander Kotov <kotov.sanek@gmail.com>
2022-06-11 04:59:08 +0700

    Private Requests configs

--- src/Contracts/Requests/PrivateRequest.php
+++ src/Contracts/Requests/PrivateRequest.php
@@ -4,5 +4,40 @@ namespace AlexanderKotov28\TradeApiPrototype\Contracts\Requests;
 
 abstract class PrivateRequest extends Request
 {
+    private string $api_id;
+    private string $api_secret_key;
 
+    abstract protected function getMethodName(): string;
+
+    public function setApiId(string $api_id): static
+    {
+        $this->api_id = $api_id;
+        return $this;
+    }
+
+    public function setApiSecretKey(string $api_secret_key): static
+    {
+        $this->api_secret_key = $api_secret_key;
+        return $this;
+    }
+
+    protected function buildParams(): array
+    {
+        return array_merge($this->getParams(), [
+            'ts' => round(microtime(true) * 1000)
+        ]);
+    }
+
+    protected function getHeaders(): array
+    {
+        return [
+            'API-ID' => $this->api_id,
+            'API-SIGN' => $this->getSign()
+        ];
+    }
+
+    protected function getSign(): string
+    {
+        return hash_hmac('sha256', $this->getMethodName() . json_encode($this->buildParams()), $this->api_secret_key);
+    }
 }
