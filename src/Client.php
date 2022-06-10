1f940bad4a0ceea2e2199b6d67a842f6e1d86a7c
Alexander Kotov <kotov.sanek@gmail.com>
2022-06-11 06:02:59 +0700

    Remove deprecated Client method request

--- src/Client.php
+++ src/Client.php
@@ -12,62 +12,11 @@ use AlexanderKotov28\TradeApiPrototype\Requests\OrderStatusRequest;
 
 class Client
 {
-    private $arError = array();
-
     private RequestFactoryInterface $request_factory;
 
     public function __construct(?string $api_id = null, ?string $api_secret_key = null)
     {
         $this->request_factory = new RequestFactory($api_id, $api_secret_key, new \GuzzleHttp\Client());
-    }
-
-    /**
-     * @param $req
-     * @return mixed
-     * @deprecated This method will be removed after HttpClient implementation
-     */
-    private function reqeust($req = array())
-    {
-        $msec = round(microtime(true) * 1000);
-        $req['post']['ts'] = $msec;
-
-        $post = json_encode($req['post']);
-
-        $sign = hash_hmac('sha256', $req['method'].$post, $this->api_secret_key);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://payeer.com/api/trade/".$req['method']);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type: application/json",
-            "API-ID: ".$this->api_id,
-            "API-SIGN: ".$sign
-        ));
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $arResponse = json_decode($response, true);
-
-        if ($arResponse['success'] !== true)
-        {
-            $this->arError = $arResponse['error'];
-            throw new Exception($arResponse['error']['code']);
-        }
-
-        return $arResponse;
-    }
-
-    public function getError()
-    {
-        return $this->arError;
     }
 
     public function info(): InfoRequest
